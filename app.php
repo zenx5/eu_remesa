22886ad37bfcac0c85c6dd9c13484b1589925057
root <root@zenx5.pro>
2023-06-11 11:14:54 +0000

    feat(app.php): add support for multiple scripts to be loaded in the app by adding a new GET parameter 'script' and looping through the scripts to include them in the HTML. Also, add a new GET parameter 'type' to specify the type of the template file to be included. Finally, add a new style to the body to improve the app's appearance.

--- app.php
+++ app.php
@@ -1,5 +1,7 @@
 <?php
     $template = $_GET['template'];
+    $type = $_GET['type'];
+    $scripts = $_GET['script'];
     $token =  $_GET['token'];
 ?>
 <!DOCTYPE html>
@@ -12,20 +14,24 @@
     <link href="https://cdn.jsdelivr.net/npm/vuetify@3.3.2/dist/vuetify.min.css" rel="stylesheet"></link>
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/vuetify@3.3.2/dist/vuetify.min.js"></script>
+    <style>
+        body{
+            padding:20px;
+            background-color:white;
+        }
+    </style>
     <script>
         sessionStorage.setItem('eu_token',"<?=$token?>");
     </script>
 </head>
 <body>
     <div id="app" style="background-color: white;">
-        <?php 
-            include './templates/'.$template.'.php';
-        ?>
+        <?php include 'templates/'.$template.'.'.$type; ?>
     </div>
+    <?php foreach(explode(',',$scripts) as $script):?>
     <script>
-        <?php 
-            include './js/'.$template.'.js';
-        ?>        
+        <?php include 'js/'.$script.'.js'; ?>
     </script>
+    <?php endforeach;?>
 </body>
 </html>
