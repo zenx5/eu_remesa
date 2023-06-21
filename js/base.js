4a85859b95a3d957ea94da4bdf7c4474031c7b43
root <root@zenx5.pro>
2023-06-21 17:27:43 +0000

    feat(base.js): add ajaxAction and newQueryString functions to handle ajax requests and query string creation respectively

--- js/base.js
+++ js/base.js
@@ -3,3 +3,18 @@ const { createVuetify } = Vuetify;
 const vuetify = createVuetify();
 
 const token = sessionStorage.getItem('eu_token')
+
+const ajaxAction = async (url, action,token, queryStringData = null) => {
+    const response = await fetch(url, {
+        method:'post',
+        headers:{
+            'Content-Type':'application/x-www-form-urlencoded'
+        },
+        body:`action=${action}&token=${token}${queryStringData?'&':''}${queryStringData}`
+    })
+    return await response.json()
+}
+
+const newQueryString = ( data ) => {
+    return Object.keys( data ).map( key => `${key}=${data[key]}`).join('&')
+}
