b28bf0df10d3d0c02362dd20e1155ffcf04824f3
root <root@zenx5.pro>
2023-06-27 04:57:23 +0000

    refactor(class-eu-ajax-suport.php): remove token validation from get_option_content method to improve separation of concerns feat(class-eu-ajax-suport.php): add request_token parameter to is_token_valid method to allow validation of any token, not just POST token.

--- classes/class-eu-ajax-suport.php
+++ classes/class-eu-ajax-suport.php
@@ -4,18 +4,15 @@ class EuAjaxData{
 
     public static $defualt_timeout = 2;
 
-    public static function is_token_valid(){
-        $token = new EuJWT($_POST['token']);
-        $data = $token->getData($_POST['token']);
+    public static function is_token_valid($request_token){
+        $token = new EuJWT($request_token);
+        $data = $token->getData($request_token);
         $diff = date_diff( $data->checkpoint, date("h:i:sa") );
         return floatval($diff->i) < self::$defualt_timeout;
     }
 
     public static function get_option_content($tag, $default_value, $is_echo = true){
-        $result = $default_value;
-        if( self::is_token_valid() ) {
-            $result = get_option($tag, $default_value);
-        }
+        $result = get_option($tag, $default_value);
         if( !$is_echo ) {
             return json_decode($result,true);
         }
